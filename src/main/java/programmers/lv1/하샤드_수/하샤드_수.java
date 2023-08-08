e34aebc6b998555625ee561291bbea814d4a5e9c
JinseonYe <aatwe24@gmail.com>
2023-08-08 23:58:18 +0900

    Lv1. 하샤드 수

---
+++ src/main/java/programmers/lv1/하샤드_수/하샤드_수.java
@@ -0,0 +1,20 @@
+package programmers.lv1.하샤드_수;
+
+import java.io.IOException;
+
+public class 하샤드_수 {
+	public boolean solution (int x) throws IOException {
+		int sum = 0;
+		int a = x;
+
+		while (a >= 1) {
+			sum += a % 10;
+			a /= 10;
+		}
+
+		if (x % sum == 0) {
+			return true;
+		} else
+			return false;
+	}
+}
