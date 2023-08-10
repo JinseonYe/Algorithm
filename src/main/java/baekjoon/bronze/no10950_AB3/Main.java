bf8b45e22f34e27d1a557c15e391fd5e1968ff45
JinseonYe <aatwe24@gmail.com>
2023-08-11 04:39:16 +0900

    Bronze V. A+B-3

---
+++ src/main/java/baekjoon/bronze/no10950_AB3/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.no10950_AB3;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
