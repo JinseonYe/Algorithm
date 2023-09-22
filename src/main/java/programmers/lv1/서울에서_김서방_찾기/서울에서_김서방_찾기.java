bef9a9cd3716ee4f09738bbee0773a2c66488a3e
JinseonYe <aatwe24@gmail.com>
2023-09-22 16:20:52 +0900

    Lv.1 서울에서 김서방 찾기

--- src/main/java/programmers/lv1/서울에서_김서방_찾기/서울에서_김서방_찾기.java
+++ src/main/java/programmers/lv1/서울에서_김서방_찾기/서울에서_김서방_찾기.java
@@ -1,4 +1,17 @@
 package programmers.lv1.서울에서_김서방_찾기;
 
 public class 서울에서_김서방_찾기 {
+	public String solution(String[] seoul) {
+		String answer = "";
+
+		int x = 0;
+
+		for(int i=0; i<seoul.length; i++) {
+			if(seoul[i].equals("Kim")) {
+				x = i;
+			}
+		}
+		answer = "김서방은 " + x + "에 있다";
+		return answer;
+	}
 }
