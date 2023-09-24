6bd0851214b97a442d395d8c137c1e060c1f8e9e
JinseonYe <aatwe24@gmail.com>
2023-09-24 21:17:21 +0900

    Lv.1 수박수박수박수박수박수?

---
+++ src/main/java/programmers/lv1/수박수박수박수박수박수/수박수박수박수박수박수.java
@@ -0,0 +1,20 @@
+package programmers.lv1.수박수박수박수박수박수;
+
+public class 수박수박수박수박수박수 {
+	public String solution(int n) {
+		String answer = "";
+		StringBuilder sb = new StringBuilder("");
+
+		for (int i=1; i<=n; i++) {
+			if (i%2 == 1) {
+				sb.append("수");
+			} else if (i%2 == 0) {
+				sb.append("박");
+			}
+		}
+
+		answer = sb.toString();
+
+		return answer;
+	}
+}
