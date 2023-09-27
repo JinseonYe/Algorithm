0a9f53e89497ecd37a0f144535876e05abafb9ac
JinseonYe <aatwe24@gmail.com>
2023-09-27 21:41:15 +0900

    Lv.1 자릿수 더하기

---
+++ src/main/java/programmers/lv1/자릿수_더하기/자릿수_더하기.java
@@ -0,0 +1,13 @@
+package programmers.lv1.자릿수_더하기;
+
+public class 자릿수_더하기 {
+	public int solution(int n) {
+		int answer = 0;
+		String s = Integer.toString(n);
+
+		for(int i=0; i<s.length(); i++){
+			answer += Integer.parseInt(s.substring(i, i+1));
+		}
+		return answer;
+	}
+}
