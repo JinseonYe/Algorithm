d3e05dc6f84456b4661ca484287b2db6a417d76d
JinseonYe <aatwe24@gmail.com>
2023-10-16 17:19:05 +0900

    Lv.1 완주하지 못한 선수

---
+++ src/main/java/programmers/lv1/완주하기_못한_선수/Solution.java
@@ -0,0 +1,23 @@
+package programmers.lv1.완주하기_못한_선수;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public String solution(String[] participant, String[] completion) {
+		String answer = "";
+		HashMap<String,Integer> map = new HashMap<>();
+
+		for(String player : participant) map.put(player, map.getOrDefault(player,0)+1);
+		for(String player : completion) map.put(player, map.get(player)-1);
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			if(entry.getValue() !=0){
+				answer = entry.getKey();
+				break;
+			}
+		}
+
+		return answer;
+	}
+}
